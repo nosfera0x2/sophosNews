c4ae37052171da7a2739d9615358a1a23a6267c2
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-06-29 13:48:50 -0500

    tracking multiple sophos news sites, via function

--- app.js
+++ app.js
@@ -4,33 +4,51 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 const app = express()
-const url = "https://www.news.sophos.com/en-us/feed"
+const url = "https://news.sophos.com/en-us/"
 const articles = []
+
+newsProviders.forEach(newsProvider => {
+    axios.get(newsProvider.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            // if searching for a specific a tag with certain text
+            // $('a:contains("climate")',html).each(function () { 
+            $('a',html).each(function () {
+                const title = $(this).title
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url,
+                    source: newsProvider.name
+                })
+            })
+        })
+
+})
+
+const newsProviders = [
+    {
+        name: 'Naked Security',
+        address: "https://nakedsecurity.sophos.com"
+    },
+    {
+        name: 'Sophos Press',
+        address: 'https://www.sophos.com/en-us/company/press'
+    },
+    {
+        name: 'Sophos News',
+        address: 'https://news.sophos.com/en-us/'
+    }
+]
 
 app.get('/',(req,res) => {
         res.json('Welcome to my Sophos News API')
 })
 
 app.get('/sophos',(req,res) => {
-
-    axios.get(url)
-        .then((response) => {
-                const html = response.data
-                const $ = cheerio.load(html)
-                
-                // if searching for a specific a tag with certain text
-                // $('a:contains("climate")',html).each(function () { 
-                
-                $('a',html).each(function () {
-                    const title = $(this).text()
-                    const url = $(this).attr('href')
-                    articles.push({
-                        title,
-                        url
-                    })
-                })
-                res.json(articles)
-        }).catch(err => console.log(err))
+    res.json(articles)
 })
 
 
